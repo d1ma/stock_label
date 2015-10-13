8e54b1b6bb885f34fd707056ed53fd3eb09ce819
Dima <brezhnd@gmail.com>
2015-10-12 21:22:15 -0700

    changed 'password':

--- auth.py
+++ auth.py
@@ -6,7 +6,7 @@ def check_auth(username, password):
     """This function is called to check if a username /
     password combination is valid.
     """
-    return username == 'admin' and password == 'ilya'
+    return username == 'admin' and password == 'nlp'
 
 def authenticate():
     """Sends a 401 response that enables basic auth"""
